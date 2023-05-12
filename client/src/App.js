e6c1cb1054312a3210dcaf579f3183f884bd9201
Ajeet Rajput <ajeet.rajput@w3villa.com>
2023-05-12 11:45:18 +0530

    blog functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import UserBlogs from "./pages/UserBlogs";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Blogs />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/my-blogs" element={<UserBlogs />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </>
